11fddc54fe3cad991dc1bf3151e15173b96ee4a1
Nguyen Tuan Dung <tuandungnguyenptit@gmail.com>
2025-12-07 02:16:09 +0700

    Update file Trò chơi so sánh bài

--- Codelab PTIT/Trò chơi so sánh bài.py
+++ Codelab PTIT/Trò chơi so sánh bài.py
@@ -1,12 +1,16 @@
 game = ('J', 'Q', 'K')
-list = []
-input = input()
-for i in input:
-    list.append(i)
+card_list = []
+while True:
+    cards = input()
+    if cards not in 'JQK':
+        break
+    else: 
+        for i in cards:
+            card_list.append(i)
 
-A = list[0]
-B = list[1]
-if game.index(A) < game.index(B):
-    print("B")
-else:
-    print("A")
+    A = card_list[0]
+    B = card_list[1]
+    if game.index(A) < game.index(B):
+        print("B")
+    else:
+        print("A") 
