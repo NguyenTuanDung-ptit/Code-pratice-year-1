3c588fd1035d8e946feeb19c3e6400f14545bf0a
Nguyen Tuan Dung <tuandungnguyenptit@gmail.com>
2025-12-07 00:23:29 +0700

    Add file Kiem tra Chuoi Co Phai La Palindrome.py

---
+++ Codelab PTIT/Kiểm tra Chuỗi Có Phải Là Palindrome.py
@@ -0,0 +1,16 @@
+import string
+
+punctuation_list = string.punctuation
+text = input()
+list = []
+strip_text = text.strip()
+
+for i in strip_text:
+    if i != " " and i not in punctuation_list:
+        list.append(i)
+
+list_reverse = list[::-1]
+if list == list_reverse:
+    print("True")
+if list != list_reverse:
+    print("False")
