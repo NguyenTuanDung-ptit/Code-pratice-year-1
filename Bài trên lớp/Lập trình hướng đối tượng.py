1d7ba5501e4cd63eacaacf3cbafd6408c0f28512
Nguyen Tuan Dung <tuandungnguyenptit@gmail.com>
2025-12-11 16:44:11 +0700

    Lap trinh huong doi tuong

---
+++ Bài trên lớp/Lập trình hướng đối tượng.py
@@ -0,0 +1,32 @@
+class Student:
+    def __init__(self, name, age, lop):
+        self.name = name
+        self.age = age
+        self.lop_hoc = lop
+    def say_hello(self):
+        print(f"Xin chào, tôi tên là {self.name}, năm nay {self.age}")
+    def info(self):
+        print(f"Thông tin của sinh viên {self.name}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Class: {self.lop_hoc}")
+        if self.age < 0:
+            print("Lỗi")
+class Study(Student):
+    def study(self):
+        print(f"{self.name} đang học Python")
+
+tuan_dung = Study("Nguyễn Tuấn Dũng", 18, "D25CQCC05-B")
+tuan_dung.say_hello()
+tuan_dung.info()
+tuan_dung.study()
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.ten = owner
+        self.__balance = balance
+    def show_balance(self):
+        print(f"Số dư của {self.ten} là: {self.__balance}")
+
+acc_1 = BankAccount("Nguyễn Tuấn Dũng", 45000)
+acc_1.show_balance
